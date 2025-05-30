2294f6de6bf222d64a8c5458e70d97fd7dd93621
Yuta0101 <148316894+Yuta0101@users.noreply.github.com>
2025-05-30 17:11:49 +0800

    Add files via upload

---
+++ scripts/constant.js
@@ -0,0 +1,34 @@
+//Ekrana basılacak butonların verisi
+export const buttonData = [
+  {
+    id: 1,
+    text: "套餐（飲料可更換）",
+    value: "套餐",
+  },
+  {
+    id: 2,
+    text: "飯麵",
+    value: "飯麵",
+  },
+  {
+    id: 3,
+    text: "蛋餅 蔥抓餅",
+    value: "蛋餅 蔥抓餅",
+  },
+  {
+    id: 4,
+    text: "手工饅頭",
+    value: "手工饅頭",
+  },
+    {
+    id: 5,
+    text: "其他",
+    value: "其他",
+  },
+    {
+    id: 6,
+    text: "飲料",
+    value: "飲料",
+  },
+
+];
